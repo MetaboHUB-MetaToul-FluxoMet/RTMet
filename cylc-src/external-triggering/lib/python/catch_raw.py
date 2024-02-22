1a53610aeaa3699a94e4d938f017709ab7066dfa
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-02-22 10:14:32 +0100

    basic workflow with file detection, done.

--- cylc-src/external-triggering/lib/python/catch_raw.py
+++ cylc-src/external-triggering/lib/python/catch_raw.py
@@ -4,21 +4,21 @@ from pathlib import Path
 from datetime import datetime
 import logging
 import yaml
+import os
+
+# DO NOT PRINT ANYTHING TO STDOUT, IT'S RESERVED TO THE RESULT OF CATCH_RAW
 
 logging.basicConfig(level=logging.DEBUG)
 
-with open('../../config.yml', 'r', encoding='utf-8') as stream:
-    try:
-        wfconfig: dict = yaml.safe_load(stream)
-        logging.debug("type of wfconfig: %s", type(wfconfig))
-        input_dir: Path = Path(wfconfig["input-directory"])
-    except yaml.YAMLError as exc:
-        print("exception: ", exc)
-
-def catch_raw(point: int) -> tuple[bool, dict]:
+def catch_raw(point: str, workflow_run_dir: str) -> tuple[bool, dict]:
     """Return True if a raw file ending in _n.raw with n the cycle number
     is present in the input_dir. Return False otherwise.
     """
+    point: int = int(point)
+    workflow_run_dir: Path = Path(workflow_run_dir)
+    
+    config_file = workflow_run_dir / "config.yml"
+    input_dir = load_input_config(config_file)
     # Files are named like "file_01.raw" from 01 to 09 and "file_10.raw" from 10 to 99
     if point <= 9:
         point: str = f'0{point}'
@@ -31,19 +31,33 @@ def catch_raw(point: int) -> tuple[bool, dict]:
                 # lockdir are created by the scp daemon when the file is being
                 # copied and are removed when the copy is finished
             ):
-            logging.debug("RAW FILE FOUND: %s", filepath)
-            return True, {'file': filepath, 'time': get_file_datetime(filepath)}
+            logging.debug("🛑 RAW FILE FOUND: %s", filepath)
+            return True, {
+                'file': str(filepath),
+                'time': get_file_datetime(filepath).strftime('%Y-%m-%d %H:%M:%S')
+            }
+            #return True, {}
     # No raw file found
-    logging.debug("No raw file found in %s", input_dir)
+    logging.debug("🛑 No raw file found in %s", input_dir)
     return False, {}
 
-def get_file_datetime(file: Path):
+def load_input_config(config_file: Path) -> Path:
+    """Should be replaced by a call to the Cylc suite's configuration
+    """
+    with open(config_file, 'r', encoding='utf-8') as stream:
+        try:
+            wfconfig: dict = yaml.safe_load(stream)
+            logging.debug("🛑 type of wfconfig: %s", type(wfconfig))
+            input_dir: Path = Path(wfconfig["input-directory"])
+        except yaml.YAMLError as exc:
+            raise exc
+        return input_dir
+
+def get_file_datetime(file: Path) -> datetime:
     """Return the last modification time of the file."""
     modification_time = file.stat().st_mtime
-    formatted_time: datetime = datetime.fromtimestamp(modification_time)
+    formatted_time: str = datetime.fromtimestamp(modification_time)
     return formatted_time
-
-catch_raw(1)
 
 # def get_raws_datetimes():
 #     """Return a dictionary of raw file names in the directory and

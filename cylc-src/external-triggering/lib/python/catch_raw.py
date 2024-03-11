7e8f18d6237128858aeadb4ebeb3b0a17ac54103
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-03-11 16:16:18 +0100

    added a variable for .RAW filename detection rule

--- cylc-src/external-triggering/lib/python/catch_raw.py
+++ cylc-src/external-triggering/lib/python/catch_raw.py
@@ -2,31 +2,33 @@
 
 from pathlib import Path
 from datetime import datetime
+from typing import Callable
 import logging
 import yaml
-import os
 
 # DO NOT PRINT ANYTHING TO STDOUT, IT'S RESERVED TO THE RESULT OF CATCH_RAW
 
 logging.basicConfig(level=logging.DEBUG)
 
 def catch_raw(point: str, workflow_run_dir: str) -> tuple[bool, dict]:
-    """Return True if a raw file ending in _n.raw with n the cycle number
-    is present in the input_dir. Return False otherwise.
+    """Return {True, {'file': Path, 'time': str} if a raw file ending in _n.raw
+    with n the cycle number is present in the input_dir.
+    Return (False, {}) otherwise.
     """
     point: int = int(point)
     workflow_run_dir: Path = Path(workflow_run_dir)
-    
-    config_file = workflow_run_dir / "config.yml"
-    input_dir = load_input_config(config_file)
-    # Files are named like "file_01.raw" from 01 to 09 and "file_10.raw" from 10 to 99
-    if point <= 9:
-        point: str = f'0{point}'
+
+    filename_rule: Callable[[int, Path], bool] = t15_naming_210
+    # DEAR USER: CHANGE USED RULE IF NEEDED, OR CODE A NEW ONE, OR CHANGE HOW
+    # YOUR FILES ARE NAMED BY THE INSTRUMENT.
+
+    config_file: Path = workflow_run_dir / "config.yml"
+    input_dir: Path = load_input_config(config_file)
+
     for filepath in input_dir.iterdir():
         if (
                 filepath.is_file() and
-                filepath.suffix == '.raw' and
-                filepath.stem.endswith(f'_{point}') and
+                filename_rule(point, filepath) and
                 not (filepath / ".lock").is_dir()
                 # lockdir are created by the scp daemon when the file is being
                 # copied and are removed when the copy is finished
@@ -36,7 +38,6 @@ def catch_raw(point: str, workflow_run_dir: str) -> tuple[bool, dict]:
                 'file': str(filepath),
                 'time': get_file_datetime(filepath).strftime('%Y-%m-%d %H:%M:%S')
             }
-            #return True, {}
     # No raw file found
     logging.debug("🛑 No raw file found in %s", input_dir)
     return False, {}
@@ -59,15 +60,17 @@ def get_file_datetime(file: Path) -> datetime:
     formatted_time: str = datetime.fromtimestamp(modification_time)
     return formatted_time
 
-# def get_raws_datetimes():
-#     """Return a dictionary of raw file names in the directory and
-#     their last modification times.
-#     """
-#     file_mod_times: dict = {}
-#     for filepath in input_dir.iterdir():
-#         if filepath.is_file() and filepath.suffix == '.raw':
-#             modification_time = filepath.stat().st_mtime
-#             formatted_time: datetime = datetime.fromtimestamp(modification_time)#\
-#                 #.strftime('%Y-%m-%d %H:%M:%S')
-#             file_mod_times[filepath] = formatted_time
-#     return file_mod_times
+def integer_naming(point: int, filepath: Path) -> bool:
+    """Return True if the file name ends with _n.raw where n is the cycle point number.\n
+    Files are named like "file_01.raw" from 01 to 09 and "file_10.raw" from 10 to 99.
+    """
+    f_point: int = f'0{point}' if point <= 9 else point
+    return filepath.stem.endswith(f'_{f_point}') and filepath.suffix == '.raw'
+
+def t15_naming(point: int, filepath: Path) -> bool:
+    """Return True if the file name ends with _t[15*n-1].raw where n is the cycle point number."""
+    return filepath.stem.endswith(f'_t{15*(point-1)}') and filepath.suffix == '.raw'
+
+def t15_naming_210(point: int, filepath: Path) -> bool:
+    """Return True if the file name ends with _t[15*n-1].raw where n is the cycle point number."""
+    return filepath.stem.endswith(f'_t{15*(point-1)+210}') and filepath.suffix == '.raw'

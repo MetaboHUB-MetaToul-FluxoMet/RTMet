4fddd0d11e75076f66d0979ff5fb5a88437c8393
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-03-20 09:21:15 +0100

    Made the workflow more generic, and added user config in the [[root]] family task definition.

--- cylc-src/external-triggering/lib/python/append_timeseries.py
+++ cylc-src/external-triggering/lib/python/append_timeseries.py
@@ -2,13 +2,16 @@
 import os
 import pandas as pd
 
-CYLC_WORKFLOW_SHARE_DIR = os.environ['CYLC_WORKFLOW_SHARE_DIR']
-CYLC_TASK_CYCLE_POINT = os.environ['CYLC_TASK_CYCLE_POINT']
+CYLC_WORKFLOW_SHARE_DIR: str = os.environ['CYLC_WORKFLOW_SHARE_DIR']
+CYLC_TASK_CYCLE_POINT: str = os.environ['CYLC_TASK_CYCLE_POINT']
+NAMES_DB: int = int(os.environ['NAMES_DB']) - 1 # 0-indexed
 
 def append():
     matches = pd.read_csv(f'{CYLC_WORKFLOW_SHARE_DIR}/{CYLC_TASK_CYCLE_POINT}/features_annotated_matches.tsv', sep='\t')
     intensity_timeseries = pd.read_csv(f'{CYLC_WORKFLOW_SHARE_DIR}/intensity_timeseries.tsv', sep='\t')
-    matches['compound'] = matches.iloc[:, 9].apply(lambda x: x.split('#')[3])
+    matches['compound'] = matches.iloc[:, 9].apply(lambda x: x.split('#')[NAMES_DB+2])
+    # /!\ sort might be resource intensive. May need to be optimized
     matches = matches.sort_values('intensity', ascending=False).drop_duplicates('compound').sort_index()
-    intensity_timeseries[str(CYLC_TASK_CYCLE_POINT)] = intensity_timeseries['NAME'].map(matches.set_index('compound')['intensity'])
+    # Dropping duplicates is necessary for the map method.
+    intensity_timeseries[str(CYLC_TASK_CYCLE_POINT)] = intensity_timeseries.iloc[:,NAMES_DB].map(matches.set_index('compound')['intensity'])
     intensity_timeseries.to_csv(f'{CYLC_WORKFLOW_SHARE_DIR}/intensity_timeseries.tsv', sep='\t', na_rep='NA', index=False)

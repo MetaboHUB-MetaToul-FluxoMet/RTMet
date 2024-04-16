2c65e17b009ea49dc4403d4a7b67b8afc80088aa
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-04-16 10:30:05 +0200

    removed BIH from workflow

--- cylc-src/bioreactor-workflow/lib/python/append_timeseries.py
+++ cylc-src/bioreactor-workflow/lib/python/append_timeseries.py
@@ -7,16 +7,6 @@ CYLC_TASK_CYCLE_POINT: str = os.environ['CYLC_TASK_CYCLE_POINT']
 NAMES_DB: int = int(os.environ['NAMES_DB']) - 1 # 0-indexed
 RAWFILE_STEM: str = os.environ['RAWFILE_STEM']
 
-def append():
-    matches = pd.read_csv(f'{CYLC_WORKFLOW_SHARE_DIR}/{CYLC_TASK_CYCLE_POINT}/features_annotated_matches.tsv', sep='\t')
-    intensity_timeseries = pd.read_csv(f'{CYLC_WORKFLOW_SHARE_DIR}/intensity_timeseries.tsv', sep='\t')
-    matches['compound'] = matches.iloc[:, 9].apply(lambda x: x.split('#')[NAMES_DB+2])
-    # /!\ sort might be resource intensive. May need to be optimized
-    matches = matches.sort_values('intensity', ascending=False).drop_duplicates('compound').sort_index()
-    # Dropping duplicates is necessary for the map method.
-    intensity_timeseries[str(CYLC_TASK_CYCLE_POINT)] = intensity_timeseries.iloc[:,NAMES_DB].map(matches.set_index('compound')['intensity'])
-    intensity_timeseries.to_csv(f'{CYLC_WORKFLOW_SHARE_DIR}/intensity_timeseries.tsv', sep='\t', na_rep='NA', index=False)
-
 def append_mzmatch():
     matches = pd.read_csv(f'{CYLC_WORKFLOW_SHARE_DIR}/{CYLC_TASK_CYCLE_POINT}/{RAWFILE_STEM}_mzmatches.tsv', sep='\t')
     intensity_timeseries = pd.read_csv(f'{CYLC_WORKFLOW_SHARE_DIR}/intensity_timeseries_mzmatch.tsv', sep='\t')

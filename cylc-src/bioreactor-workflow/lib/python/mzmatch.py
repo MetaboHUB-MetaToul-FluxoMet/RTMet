b7c77e5dd7cbf9cca76026156bcdff8c536c9f5b
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-05-03 11:06:23 +0200

    mzmatch use polarity for matching

--- cylc-src/bioreactor-workflow/lib/python/mzmatch.py
+++ cylc-src/bioreactor-workflow/lib/python/mzmatch.py
@@ -1,4 +1,6 @@
+import math
 from pathlib import Path
+
 import pandas as pd
 
 pd.set_option("display.max_columns", None)
@@ -37,20 +39,26 @@ def match(db_path: Path, query_path: Path, mz_tol: float) -> None:
         f"{query_dir}/{query_stem}.features.annotated.csv"
     )
 
-    database[pol_db] = database[pol_db].map({1: "p", 2: "p", -1: "n", -2: "n"})
-    # database[mz_db] = database[mz_db].apply(pd.to_numeric)
-    # query[[mz_q, intensity_q]] = query[[mz_q, intensity_q]].apply(pd.to_numeric)
+    database[pol_db] = database[pol_db].apply(lambda x: math.copysign(1, x))
+    database[pol_db] = database[pol_db].map({1: "p", -1: "n"})
 
     database["MASS_MIN"] = database[mz_db] - database[mz_db] * tol_ppm / 1e6
     database["MASS_MAX"] = database[mz_db] + database[mz_db] * tol_ppm / 1e6
 
     combinaisons = query.merge(database, how="cross")
 
-    matches = combinaisons.query(f"(MASS_MIN <= {mz_q}) & ({mz_q} <= MASS_MAX)")
+    matches = combinaisons.query(
+        f"(MASS_MIN <= `{mz_q}`) & (`{mz_q}` <= MASS_MAX) & (`{pol_q}` == `{pol_db}`)"
+    )
     matches["delta_ppm"] = (matches[mz_q] - matches[mz_db]) / matches[mz_db] * 1e6
 
-    matches = matches[[ids_db, mz_q, "delta_ppm", intensity_q]].rename(
-        columns={ids_db: "isobaric_id", mz_q: "feature_mz", intensity_q: "intensity"}
+    matches = matches[[ids_db, mz_q, pol_q, "delta_ppm", intensity_q]].rename(
+        columns={
+            ids_db: "isobaric_id",
+            mz_q: "feature_mz",
+            pol_q: "feature_pol",
+            intensity_q: "intensity",
+        }
     )
 
     query["annotated"] = query[mz_q].isin(matches["feature_mz"])

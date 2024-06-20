2f086f3a0c62a2333b0da350ecc97dbf1be3613c
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-06-20 10:55:49 +0200

    add rose and cylc dependencies to sphinx / readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+import metomi.rose
+
 project = "RTMet"
 copyright = "2024, MetaToul"
 author = "Elliot Fontaine"
@@ -16,7 +21,17 @@ version = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# Add local sphinx extensions directory to extensions path.
+sys.path.append(os.path.abspath("ext"))
+
+extensions = [
+    # cylc.sphinx_ext extensions (from cylc.sphinx_ext-extensions library)
+    "cylc.sphinx_ext.cylc_lang",
+    # Custom extensions (in ext/ directory) made for Rose (MET Office)
+    "rose_lang",
+    "rose_domain",
+]
+# extensions = ["cylc.sphinx_ext.cylc_lang"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -26,10 +41,14 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 html_logo = "_static/logo_white.png"
 html_theme_options = {
     "logo_only": True,
     "display_version": False,
     "style_external_links": True,
 }
+
+
+def setup(app):
+    app.config.html_static_path.append("_static")

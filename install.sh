6462b414801aa7feed38dc576e3376f2ef6b7cc3
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-07-24 16:22:14 +0200

    run `conda init` after miniforge installation

--- install.sh
+++ install.sh
@@ -123,6 +123,7 @@ _installMiniforge_() {
     URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
     curl -LO "${URL}" ${VERBOSE:+--verbose}
     bash Miniforge3-"$(uname)"-"$(uname -m)".sh -b
+    conda init "$(basename "$SHELL")" ${VERBOSE:+--verbose}
 }
 
 _setupCylcWrapper_() {
